3b8c2654874dbc335c557697c1fed21caf1026a0
NikKirsanov <120027704+NikKirsanov@users.noreply.github.com>
2025-04-07 11:37:34 +0300

    Update SQLlaused.sql

--- SQLlaused.sql
+++ SQLlaused.sql
@@ -12,7 +12,7 @@ USE nikitalogitpv23;
 --identity(1,1) - ise täidad tabeli 1,2,3,...
 CREATE TABLE inimene(
 inimeneID int Primary Key identity(1,1) 
-nimi varchar(50) unique,
+nimi varchar(50) unique
 synniaeg date,
 telefon char(12),
 pikkus decimal(5,2),
@@ -23,3 +23,20 @@ SELECT * FROM inimene;
 
 --tabeli kustutamine
 DROP table inimene;
+
+--andmete lisamine
+--DDL - data 
+--DML -data manipilation language
+
+INSERT INTO inimene
+(nimi, synniag, telefon, pikkus, opilaskodu)
+VALUES
+('Peeter Uss', '2021-12-30', '2568952' , 90.5, 0),
+('Kaarel Oja', '2021-12-30', '2568952' , 90.5, 0),
+('Mati kaskk', '2021-12-30', '2568952' , 140.5, 0),
+('Peeter Vana', '2021-12-30', '2568952' , 190.5, 0),
+
+SELECT * FROM inimene;
+
+DELETE FROM inimene
+WHERE inimeneID=3;
