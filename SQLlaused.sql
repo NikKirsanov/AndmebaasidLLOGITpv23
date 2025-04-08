8f3ec0657b33e8a3a1fc8fad9379907478771761
NikKirsanov <120027704+NikKirsanov@users.noreply.github.com>
2025-04-08 14:40:02 +0300

    Update SQLlaused.sql

--- SQLlaused.sql
+++ SQLlaused.sql
@@ -114,3 +114,29 @@ ALTER TABLE inimene
 ADD constraint fk_auto
 FOREIGN KEY (autoID)
 references auto(autoID);
+
+CREATE TABLE loom(
+loomID int PRIMARY KEY AUTO_INCREMENT,
+nimi varchar(50),
+kaal decimal(2,2)
+);
+
+
+CREATE TABLE tyyp(
+tyypID int PRIMARY KEY AUTO_INCREMENT,
+tyyp varchar(50),
+kirjeldus varchar(70)
+);
+INSERT INTO tyyp(tyyp, kirjeldus)
+VALUES ('taksa','asub tallinnas');
+
+ALTER TABLE loom ADD tyypID int;
+
+
+INSERT INTO tyyp(tyyp, kirjeldus)
+VALUES ('kass','musta kass');
+
+
+ALTER TABLE loom
+ADD constraint fk_tyyp
+не доделана
