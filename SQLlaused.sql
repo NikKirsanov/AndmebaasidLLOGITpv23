e48fbed4d95b1949f8ea889ae255a3c02f4672a5
NikKirsanov <120027704+NikKirsanov@users.noreply.github.com>
2025-04-08 12:07:52 +0300

    Update SQLlaused.sql

--- SQLlaused.sql
+++ SQLlaused.sql
@@ -1,18 +1,19 @@
 --kommentaar
---sql sever managment stuudio
---conect TO:
---Server Name: --(localdb)\mssqllocaldb
--- Authentification 2 tüüpi:
---1. Windows Auth - localdb admini õigused
---2. SQL Server Aurh - kontrollida varem tehtud kasutajad
-CREATE DATABASE nikitalogitpv23;
-USE nikitalogitpv23;
+--SQL Server Management Studio
+--Connect TO:
+--Server Name: (localdb)\MSSQLLocalDB
+- Authentification 2 tüüpi:
+--1. Windows Authentification - localdb admini õigused
+--2. SQL Server Auth - kontrollida varem tehtud kasutajad
+
+CREATE DATABASE ashurovLOGITpv23;
+USE ashurovLOGITpv23;
 
 --tabeli loomine
---identity(1,1) - ise täidad tabeli 1,2,3,...
+--identity(1,1) - ise täidab tabeli 1,2,3...
 CREATE TABLE inimene(
-inimeneID int Primary Key identity(1,1) 
-nimi varchar(50) unique
+inimeneID int Primary Key identity(1,1),
+nimi varchar(50) unique,
 synniaeg date,
 telefon char(12),
 pikkus decimal(5,2),
@@ -20,23 +21,22 @@ opilaskodu bit
 );
 SELECT * FROM inimene;
 
-
 --tabeli kustutamine
 DROP table inimene;
 
 --andmete lisamine
---DDL - data 
---DML -data manipilation language
+--DDL - data definiton language
+--DML - data manipulation language
 
 INSERT INTO inimene
-(nimi, synniag, telefon, pikkus, opilaskodu)
+(nimi, synniaeg, telefon, pikkus, opilaskodu)
 VALUES
-('Peeter Uss', '2021-12-30', '2568952' , 90.5, 0),
-('Kaarel Oja', '2021-12-30', '2568952' , 90.5, 0),
-('Mati kaskk', '2021-12-30', '2568952' , 140.5, 0),
-('Peeter Vana', '2021-12-30', '2568952' , 190.5, 0),
-
-SELECT * FROM inimene;
-
+('Peeter Uus', '2021-12-30', '2568952', 90.5, 0),
+('Kaarel Oja', '2000-12-30', '2568952', 90.5, 0),
+('Kask Mati', '1983-12-30', '33333', 140.5, 0),
+('Peeter Oja', '2021-12-30', '333333', 190.5, 0)
+;
+SELECT * FROM inimene; 
+--kustuta id=2
 DELETE FROM inimene
-WHERE inimeneID=3;
+WHERE inimeneID=2;
